7af256da9d4c4e944b66db736b7b06fec4d3103c
KimYoungSoo <qhdl301@gmail.com>
2021-01-12 23:19:23 +0900

    Hook에 useState() 사용
    - Counter.js로 숫자 더하기 뺴기로 상태 값 관리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true}/>  {/* Boolean 값은 별도에 Value 설정을 안하면 True로 인식*/}
-      <Hello color="pink" />
-    </Wrapper>
-  )
+    
+    <Counter />
+    
+  );
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Counter(){
+    // useState()는 함수형 업데이트
+    // useState()의 ()안에 값은 default 값 셋팅
+    // useState()는 사용하게 되면 배열로 반환
+    // 첫번째 원소는 number(현재 상태 값), 두번째 원소 setNumber(현재 상태 값을 변경하는 함수)
+    const [number,setNumber] = useState(0); // 배열 비구조화 
+
+    const onIncrease = () => {
+        // setNumber(number + 1); Case 1. 기본 업데이트(기본 문법)
+        setNumber(prevNumber => prevNumber + 1); // Case 2. 함수형 업데이트(최적화 문법)
+    };
+
+    const onDecrease = () => {
+        // setNumber(number - 1);   Case 1. 기본 업데이트(기본 문법)
+        setNumber(prevNumber => prevNumber - 1); // Case 2. 함수형 업데이트(최적화 문법)
+    };
+
+    return (
+
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
+        </div>
+
+    );
+
+}
+
+export default Counter;
